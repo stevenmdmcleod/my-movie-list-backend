087b45c248c431f497f2fe65c64c02d6591c46d4
lucia <lucia12onica@gmail.com>
2025-03-26 08:04:41 -0500

    added register functionality

--- controller/userController.js
+++ controller/userController.js
@@ -7,5 +7,23 @@ const { authenticateToken } = require("../util/jwt");
 require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
+router.post("/register", validateUserData, async (req, res) => {
+    try {
+        const data = await userService.createUser(req.body);
+        res.status(201).json("Registration successful.");
+    } catch (err) {
+        logger.error(`Error registering user: ${err.message}`);
+        res.status(400).json(err.message);
+    }
+});
+
+function validateUserData(req, res, next) {
+    const data = req.body;
+    if(data.username && data.password && data.email) {
+        next();
+    } else {
+        res.status(400).json("Username, email and password required");
+    }
+}
 
 module.exports = router;
--- service/userService.js
+++ service/userService.js
@@ -1,3 +1,68 @@
 const userDao = require("../repository/userDAO");
 const bcrypt = require("bcrypt");
 const uuid = require('uuid');
+const logger = require("../util/logger");
+
+async function createUser(user) {
+    
+    try { 
+
+        if(!validateEmail(user.email)) {
+            logger.info("Email is not valid");
+            throw new Error("Email is not valid");
+        }
+        
+        if(!validateUser(user)) {
+            logger.info("Username and Password must be longer than 7 characters");
+            throw new Error("Username and Password must be longer than 7 characters");
+        }
+
+        const userByUsername = await userDao.getUserByUsername(user.username);
+
+        if (userByUsername) {
+            logger.info("Username already exists");
+            throw new Error("Username already exists");
+        }
+
+        const userByEmail = await userDao.getUserByEmail(user.email);
+
+        if (userByEmail) {
+            logger.info("Email already exists");
+            throw new Error("Email already exists");
+        }
+
+        const saltRounds = 10;
+        const hashPass = await bcrypt.hash(user.password, saltRounds);
+        const userId = uuid.v4();
+        const result = await userDao.createUser({
+            userId, 
+            username: user.username, 
+            password: hashPass, 
+            email: user.email,
+            isAdmin: false,
+            isBanned: false,
+            profilePicture: "",
+            biography: "",
+            preferredGenres: [],
+            friends: [],
+            recentlyAdded: []
+        });
+        logger.info(`User successfully created: ${user.username}`);
+        return result;
+    } catch (err) {
+        logger.error(`Error in postUser: ${err.message}`);
+        throw err;
+    }    
+}
+
+function validateUser(user) {
+    return user.username.length > 7 && user.password.length > 7;
+}
+
+function validateEmail(email) {
+    var re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+}
+
+
+module.exports = {createUser}
