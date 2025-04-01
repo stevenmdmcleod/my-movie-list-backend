277a0ea28375ac0da77ab597d3ccb1d49e6914de
Steven <stevenmdmcleod@gmail.com>
2025-04-01 19:34:14 -0400

    created createWatchlist function in watchlistDao

--- repository/watchlistDAO.js
+++ repository/watchlistDAO.js
@@ -1,1 +1,25 @@
 const {dbClient, s3} = require("../util/config");
+const logger = require("../util/logger");
+const {PostCommand, PutCommand, QueryCommand, GetCommand, UpdateCommand, DeleteCommand} = require("@aws-sdk/lib-dynamodb");
+
+const TableName = 'my-movie-list-watchlists';
+
+
+async function createWatchlist(list) {
+    const command = new PostCommand({
+        TableName,
+        Item: list
+    })
+
+    try {
+        const data = await dbClient.send(command);
+        logger.info(`Post command to database complete ${JSON.stringify(data)}`);
+        return list;
+    } catch (error) {
+        logger.log(error);
+        throw new Error("DAO: failed to create list");
+    }
+}
+
+
+module.exports = {createWatchlist}
--- service/watchlistService.js
+++ service/watchlistService.js
@@ -1,2 +1,43 @@
 const watchlistDao = require("../repository/watchlistDAO");
 const uuid = require('uuid');
+const logger = require("../util/logger");
+
+async function createWatchlist(userId, listName) {
+    
+    try { 
+
+        if (userByUsername) {
+            logger.info("Username already exists");
+            throw new Error("Username already exists");
+        }
+
+        const userByEmail = await userDao.getUserByEmail(user.email);
+
+        if (userByEmail) {
+            logger.info("Email already exists");
+            throw new Error("Email already exists");
+        }
+
+        
+        const listId = uuid.v4();
+        const result = await watchlistDao.createWatchlist({
+            listId,
+            userId: userId,
+            listName: listName,
+            collaborators: [],
+            likes: [],
+            titles: [],
+            comments: [],
+            isPublic: true
+            
+
+
+
+        });
+        logger.info(`List successfully created: ${user.username}`);
+        return result;
+    } catch (err) {
+        logger.error(`Error in PostList: ${err.message}`);
+        throw err;
+    }    
+}
