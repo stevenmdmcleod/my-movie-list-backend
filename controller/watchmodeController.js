63b014b642ddb2cefe53543c939ad2d264dc7ecc
nbowd <nickbwdn@gmail.com>
2025-04-13 20:12:24 -0700

    Added /watchmode/title/:titleId route for frontend to retrieve title information from watchmode API

---
+++ controller/watchmodeController.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+require('dotenv').config();
+const logger = require("../util/logger");
+
+router.get('/title/:titleId', async (req, res) => {
+    try {
+        const response = await fetch(`https://api.watchmode.com/v1/title/${req.params.titleId}/details/?apiKey=${process.env.WATCHMODE_API_KEY}`)
+        if (response.status === 200) {
+            const responseJSON = await response.json();
+            return res.status(200).json(responseJSON)
+        }
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+})
+
+module.exports = router;
